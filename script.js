f403410896381bc0c269f43327fd4573a06aceed
Vonethasin <157877009+vonethasin07@users.noreply.github.com>
2024-07-30 22:53:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function menubar(x) {
+    x.classList.toggle("change");
+
+    let myMenu = document.getElementById("myMenu");
+    if (myMenu.className === 'menu') {
+        myMenu.className += ' menu-active'
+    } else {
+        myMenu.className = 'menu'
+    }
+  }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const headerOffset = 100;
+        const elementPosition = target.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+        smoothScrollTo(offsetPosition, 800); // 800 milliseconds duration
+    });
+});
+
+function smoothScrollTo(targetPosition, duration) {
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
